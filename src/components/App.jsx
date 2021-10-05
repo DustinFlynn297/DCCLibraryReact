2b6ea4a470c83978432e8407b29b907bc4720a4d
Dustin Flynn <flynnd297@gmail.com>
2021-10-05 13:50:09 -0500

    Added previous and next book functionality for buttons within our app.jsx file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,58 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};        
+        this.books = [
+            {title: "Ready Player One", author: "Ernest Cline"},
+            {title: "All the Light We Cannot See", author: 'Anthony Doerr'},
+            {title: "The First and last Freedom", author: "Jiddu Krishnamurit"}
+        ]
+        this.state = {
+            bookNumber: 0
+        };        
+    }
+
+    gotToNextBook = () => {
+        debugger;
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        })
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
 
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="row">
+                    <div className="col-md-4">
+                        {/*Button here to move to the pervious book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
+                    </div>
+                    <div className="col-md-4">
+                        {/*Display book with cover here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4"> 
+                        {/*Button here to move to the pervious book viewed*/}
+                        <button onClick={this.gotToNextBook}>Next Book</button>
+                    </div>
+                </div>
             </div>
         )
     }
