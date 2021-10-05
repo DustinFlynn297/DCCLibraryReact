4ea91e0e5437dc96fde54afd65b6bc25f7bab462
Dustin Flynn <flynnd297@gmail.com>
2021-10-05 12:54:17 -0500

    Created an app component with a css and jsx file. Also created a titlebar component with a css and jsx file.

---
+++ src/components/TitleBar/TitleBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TitleBar.css'
+
+function TitleBar(props){
+    return (
+        <div className ="row row-space">
+            <div className="col-md-12" style={{ padding: 0 }}>
+                <div className='titlebar-nav'>
+                    <h1>Book Library</h1>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default TitleBar;
